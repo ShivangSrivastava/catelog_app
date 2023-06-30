4ca0d4a63ab6940a5683534955200774a63b741b
ShivangSrivastava <shivangsrivastava157@gmail.com>
2023-07-01 03:42:07 +0530

    day12

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:catelog_app/models/catalog.dart';
 import 'package:catelog_app/widgets/drawer.dart';
+import 'package:catelog_app/widgets/item_widgets.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,15 +10,22 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(5, (int index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Catalog App',
         ),
       ),
-      body: Center(
-        child: Container(
-          child: Text('Hello World Day 2'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
