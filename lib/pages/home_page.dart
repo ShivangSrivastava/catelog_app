5b9d8d85783e26896ca5603732c4ce0c705d9a94
ShivangSrivastava <shivangsrivastava157@gmail.com>
2023-07-01 10:21:08 +0530

    day13

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,33 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:convert';
+
 import 'package:catelog_app/models/catalog.dart';
 import 'package:catelog_app/widgets/drawer.dart';
 import 'package:catelog_app/widgets/item_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJSON = await rootBundle.loadString('assets/files/catalog.json');
+    var decodedData = jsonDecode(catalogJSON);
+    var productsData = decodedData["products"];
+    
+  }
 
   @override
   Widget build(BuildContext context) {
